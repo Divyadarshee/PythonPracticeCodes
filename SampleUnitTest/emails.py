d0bea1e791af23b5d35a2500c1a8dab136b8cb59
Divyadarshee Das <divyadarsheedas@gmail.com>
2021-12-10 14:00:21 +0530

    Update emails.py
    fixed the code in case of non-existent users with the message output: "No email address found"

--- SampleUnitTest/emails.py
+++ SampleUnitTest/emails.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-
-import sys
 import csv
-
+import sys
 def populate_dictionary(filename):
   """Populate a dictionary with name/email pairs for easy lookup."""
   email_dict = {}
@@ -12,21 +10,21 @@ def populate_dictionary(filename):
       name = str(row[0].lower())
       email_dict[name] = row[1]
   return email_dict
-
 def find_email(argv):
   """ Return an email address based on the username given."""
   # Create the username based on the command line input.
   try:
     fullname = str(argv[1] + " " + argv[2])
-  # Preprocess the data
-    email_dict = populate_dictionary('/home/student-02-8fbf4779b6bc/data/user_em                                                                                        ails.csv')
-  # Find and print the email
+    # Preprocess the data
+    email_dict = populate_dictionary('/home/{{ username }}/data/user_emails.csv')
+     # If email exists, print it
+    if email_dict.get(fullname.lower()):
+      return email_dict.get(fullname.lower())
+    else:
+      return "No email address found"
   except IndexError:
     return "Missing parameters"
-  return email_dict.get(fullname.lower())
-
 def main():
   print(find_email(sys.argv))
-
 if __name__ == "__main__":
   main()
