dfc3070a5a76027a7639b4ff8d8aa3d2ddf82c8f
Divyadarshee Das <divyadarsheedas@gmail.com>
2021-12-10 13:42:26 +0530

    Create emails.py
    populate_dictionary(filename) and find_email(argv). The function populate_dictionary(filename) reads the user_emails.csv file and populates a dictionary with name/value pairs. The other function, find_emails(argv), searches the dictionary created in the previous function for the user name passed to the function as a parameter. It then returns the associated email address. This script accepts employee's first name and last name as command-line arguments and outputs their email address.

---
+++ SampleUnitTest/emails.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import csv
+
+def populate_dictionary(filename):
+  """Populate a dictionary with name/email pairs for easy lookup."""
+  email_dict = {}
+  with open(filename) as csvfile:
+    lines = csv.reader(csvfile, delimiter = ',')
+    for row in lines:
+      name = str(row[0].lower())
+      email_dict[name] = row[1]
+  return email_dict
+
+def find_email(argv):
+  """ Return an email address based on the username given."""
+  # Create the username based on the command line input.
+  try:
+    fullname = str(argv[1] + " " + argv[2])
+  # Preprocess the data
+    email_dict = populate_dictionary('/home/student-02-8fbf4779b6bc/data/user_em                                                                                        ails.csv')
+  # Find and print the email
+  except IndexError:
+    return "Missing parameters"
+  return email_dict.get(fullname.lower())
+
+def main():
+  print(find_email(sys.argv))
+
+if __name__ == "__main__":
+  main()
